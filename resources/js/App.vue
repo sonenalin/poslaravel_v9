3bad185e199fed847222c102b6cb06664cffe21a
sonenalin <khamsonekfc@gmail.com>
2023-07-11 20:40:56 +0700

    upload to github

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Hello Vue App</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Minipos10App',
+
+    data() {
+        return {
+
+        };
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
